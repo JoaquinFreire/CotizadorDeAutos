f3eebe4539c4bb41d1cbd8f08c8889be05e6063c
Joaquin Freire <ernestojoaquinfreire@gmail.com>
2023-07-26 13:06:26 -0300

    Commit principal, deployment

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { CotizadorProvider } from "./context/CotizadorProvider";
+import AppSeguro from "./components/AppSeguro";
+import useCotizador from "./hooks/useCotizador";
+
+function App() {  
+
+  return (
+    <CotizadorProvider>
+      <AppSeguro />
+    </CotizadorProvider>
+  )
+}
+
+export default App
